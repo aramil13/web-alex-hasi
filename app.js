94e933b03351b7f0d584dd312d0250c274fcd9f0
Víctor Alonso <labra13@gmail.com>
2026-08-06 09:28:51 +0200

    cambio About us

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ const sectionObserver = new IntersectionObserver((entries) => {
         link.style.background = '';
         if (link.getAttribute('href') === `#${entry.target.id}`) {
           link.style.color = 'var(--cyan)';
-          link.style.background = 'rgba(6,182,212,0.1)';
+          link.style.background = 'rgba(200,145,62,0.1)';
         }
       });
     }
